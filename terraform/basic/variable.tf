a2d1b8bf6d5ffc72ef5612e79414f2fadbfab063
Sontn <sontn1988@gmail.com>
2019-10-10 09:09:45 +0700

    study

---
+++ terraform/basic/variable.tf
@@ -0,0 +1,37 @@
+#Define variables
+variable "env" {
+  description = "env: dev or prod"
+}
+
+variable "container_name" {
+  type        = "map"
+  description = "Name of container"
+  default     = {
+    dev   = "blog_dev"
+    prod  = "blog_prod"
+  }
+}
+
+variable "image_name" {
+  type        = "map"
+  description = "Image for container"
+  default     = {
+    dev   = "ghost:latest"
+    prod  = "ghost:alpine"
+  }
+} 
+
+variable "int_port" {
+  description = "Internal port for container"
+  default     = "2368"
+}
+
+variable "ext_port" {
+  type        = "map"
+  description = "External port for container"
+  default     = {
+    dev   = "8080"
+    prod  = "80"
+  }
+}
+
